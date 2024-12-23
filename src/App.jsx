f88bf26a45b3e0caa3e70640b1553e7a007e97fa
George-25 <georgemallory26@gmail.com>
2024-12-23 00:21:33 -0500

    app.js date&time exercise

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import "./App.css";
 
 function App() {
   let message = "Bye There";
+  const date = new Date();
+  const time = date.toLocaleTimeString();
   if (Math.random() > 0.5) {
     message = "Hello there!";
   }
   return (
     <>
       <div>{message}</div>
+      <div>{time}</div>
     </>
   );
 }
