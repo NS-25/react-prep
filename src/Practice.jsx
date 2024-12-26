97975f49665654dfd7eab4488bab7676abcdd31d
George-25 <georgemallory26@gmail.com>
2024-12-25 22:32:24 -0500

    add practice.jsx for prep

---
+++ src/Practice.jsx
@@ -0,0 +1,22 @@
+const Prep = () => {
+  const count = 10;
+  const name = "George";
+  const booleanTrue = true;
+  const booleanFalse = false;
+  const nullValue = null;
+  const undefinedValue = undefined;
+  return (
+    <>
+      <ul>
+        <li>Number: {count}</li>
+        <li>String: {name}</li>
+        <li>True: {booleanTrue}</li>
+        <li>False: {booleanFalse}</li>
+        <li>Null: {nullValue}</li>
+        <li>UndefinedValue: {undefinedValue}</li>
+      </ul>
+    </>
+  );
+};
+
+export default Prep;
