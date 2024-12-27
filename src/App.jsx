0c0e5105dabcc1b5dd8371694f077e8dd1a8939c
George-25 <georgemallory26@gmail.com>
2024-12-26 21:21:11 -0500

    create profile card in jsx src

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./App.css";
+import ProfileCard from "./ProfileCard";
 
 function App() {
-  let message = "Bye There";
-  // const date = new Date();
-  // const time = date.toLocaleTimeString();
-  if (Math.random() > 0.5) {
-    message = "Hello there!";
-  }
+  // let message = "Bye There";
+  // // const date = new Date();
+  // // const time = date.toLocaleTimeString();
+  // if (Math.random() > 0.5) {
+  //   message = "Hello there!";
+  // }
   return (
     <>
+      <div>Personal Digital Assistants</div>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
       {/* <div>{message}</div> */}
       {/* <div>{time}</div> */}
-      <div>{new Date().toLocaleTimeString()}</div>
+      {/* <div>{new Date().toLocaleTimeString()}</div> */}
     </>
   );
 }
---
+++ src/ProfileCard.jsx
@@ -0,0 +1,4 @@
+function ProfileCard() {
+  return <div>Profille Card</div>;
+}
+export default ProfileCard;
