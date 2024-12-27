b1733c1aaf496fa01d64c0d26a024ae570a6cb03
George-25 <georgemallory26@gmail.com>
2024-12-26 22:01:20 -0500

    props destructuring

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,8 +1,14 @@
-function ProfileCard(props) {
+function ProfileCard({ title, handle }) {
+  // first way
+  // const title = props.title;
+  // const handle = props.handle;
+
+  // second way to destructuring props
+  // const { title, handle } = props;
   return (
     <div>
-      <div>Title is {props.title}</div>
-      <div>Handle is {props.handle}</div>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
     </div>
   );
 }
