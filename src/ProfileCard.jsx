604a306506ce923f32a6f54185316e77ab67e186
George-25 <georgemallory26@gmail.com>
2024-12-28 12:48:14 -0500

    description done

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,4 +1,4 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description   }) {
   // first way
   // const title = props.title;
   // const handle = props.handle;
@@ -18,6 +18,7 @@ function ProfileCard({ title, handle, image }) {
           <p className="title is-4">Title is {title}</p>
           <p className="subtitle is-6">Handle is {handle}</p>
         </div>
+        <div className="content">{description}</div>
       </div>
     </div>
   );
